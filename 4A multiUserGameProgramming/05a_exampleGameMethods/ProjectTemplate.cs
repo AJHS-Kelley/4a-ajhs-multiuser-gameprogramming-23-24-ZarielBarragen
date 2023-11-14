d2fab5ef8ceac65b0d2a59b16573f040af1a1d6d
Isaiah R <s11670916@students.duvalschools.org>
2023-11-14 13:43:47 -0500

    Create ProjectTemplate.cs

---
+++ 4A multiUserGameProgramming/05a_exampleGameMethods/ProjectTemplate.cs
@@ -0,0 +1,14 @@
+// Isaiah Reyes, Project Template, v0.1
+using System;
+
+namespace UPDATEFOREACHPROGRAM
+{
+    class UDATEFOREACHPROGRAM
+    {
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
