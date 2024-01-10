7c60bddf5429c8b95c9e7f287e8c339d63bd4235
Isaiah R <s11670916@students.duvalschools.org>
2024-01-10 14:04:24 -0500

    Update ProjectTemplate.cs

--- 4A multiUserGameProgramming/06_Review/ProjectTemplate.cs
+++ 4A multiUserGameProgramming/06_Review/ProjectTemplate.cs
@@ -1,13 +1,44 @@
 // Isaiah Reyes, Project Template, v0.1
 using System;
 
-namespace UPDATEFOREACHPROGRAM
+namespace ReviewProject
 {
-    class UDATEFOREACHPROGRAM
+    class ReviewProject
     {
         
         static void Main(string[] args)
         {
+            // Declaring and Assigning Variables
+            // Interger -- -2,147,483,648 to 2,147,483,647
+            // Interger = 4 bytes
+            int yourMom = 10;
+
+            // long -- -9,223,372,936,854,775,808 to 9,223,372,936,854,775,808
+            // Long = 8 btyes
+            long ayoQuestionMark = 123456789101112131415;
+
+            //Float -- Stores up to 6-7 Decimal digits
+            //Float = 4 bytes
+            float yourDad = 12.4F;
+
+            // Double -- Stores up to 15 decimal points.
+            //Double = 8 Bytes
+            double trouble = 5.5002500600D;
+
+            // Boolean -- True / false
+            // Boolean = 1 bit
+            bool yourGirlfriend = false;
+
+            // String
+            // 2-bytes PER CHARACTER
+            // Must be in DOUBLE QOUTES "  "
+            string yourDog "Probably dead";
+
+            // Character (char)
+            // Single character, in SINGLE QOUTES ' '
+            // 2 Bytes
+            char yourGrade = 'F';
+            
             
         }
     }
