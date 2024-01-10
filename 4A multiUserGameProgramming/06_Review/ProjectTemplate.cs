6bbf9704e68ffb6f5cd3c587134d1dfd61254ec9
Isaiah R <s11670916@students.duvalschools.org>
2024-01-10 14:34:19 -0500

    Update ProjectTemplate.cs

--- 4A multiUserGameProgramming/06_Review/ProjectTemplate.cs
+++ 4A multiUserGameProgramming/06_Review/ProjectTemplate.cs
@@ -15,7 +15,7 @@ namespace ReviewProject
 
             // long -- -9,223,372,936,854,775,808 to 9,223,372,936,854,775,808
             // Long = 8 btyes
-            long ayoQuestionMark = 123456789101112131415;
+            long ayoQuestionMark = 12345678910111213;
 
             //Float -- Stores up to 6-7 Decimal digits
             //Float = 4 bytes
@@ -32,13 +32,40 @@ namespace ReviewProject
             // String
             // 2-bytes PER CHARACTER
             // Must be in DOUBLE QOUTES "  "
-            string yourDog "Probably dead";
+            string yourDog = "Probably dead";
+            string yourCat = "Ran away";
 
             // Character (char)
             // Single character, in SINGLE QOUTES ' '
             // 2 Bytes
             char yourGrade = 'F';
+
+            // Type Casting -- Changing one data type to another
+            // IMPLICIT -- Happens automatically wen going smaller -> Larger
+            // char -> int -> long -> float ->  double
+
+            int yourSister = 9000;
+            double ohNvm = yourSister;
+            Console.WriteLine(yourSister);
+
+            // EXPLICIT -- Must be done manually when going larger -> smaller
+            // double -> float -> long -> int -> char
+            double dogs = 1.2345678910D;
+            int noDogs = (int) dogs;
             
+            // Conversion Methods
+            Convert.ToString() // String
+            Convert.ToDouble()  // Double
+            Convert.ToInt32() // Integer
+            Convert.ToInt64() // Long
+
+
+             string intToString = Convert.ToString(noDogs);
+             double stringToDouble = Convert.ToDouble(yourCat);
+             int stringToInt = Convert.ToInt32(yourDog);
+             long floatToLong = Convert.ToInt64(yourDad);
+
+
             
         }
     }
